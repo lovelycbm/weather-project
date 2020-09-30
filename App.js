ee0bc9b1a11c6ae66797d88d948de2699a8506a3
lovelycbm <lovelycbm@naver.com>
2020-10-01 00:11:17 +0900

    weather_api

--- App.js
+++ App.js
@@ -1,33 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import Loading from "./Loading"
-import * as Location from "expo-location"
-import {Alert} from "react-native"
-export default class extends React.Component{
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import Loading from "./Loading";
+import * as Location from "expo-location";
+import { Alert } from "react-native";
+import axios from "axios";
+import Weather from "./Weather";
 
+const API_KEY = "0ffcb7ed0068899726af3124e8fdac04";
+
+export default class extends React.Component {
   state = {
-      isLoading : true
+    isLoading: true,
   };
 
-  getLocation = async() =>{    
+  getWeather = async (latitude, longitude) => {
+    const {
+      data: {
+        main: { temp },
+        weather,
+      },
+    } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    );
+    this.setState({
+      isLoading: false,
+      condition: weather[0].main,
+      temp,
+    });
+  };
+
+  getLocation = async () => {
     try {
-      
-      await Location.requestPermissionsAsync();      
-      const {coords : {latitude, longitude}} = await Location.getCurrentPositionAsync();
-      this.setState({isLoading:false});
-
+      await Location.requestPermissionsAsync();
+      const {
+        coords: { latitude, longitude },
+      } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
     } catch (error) {
-      Alert.alert("Can't find you","So Sad")
+      Alert.alert("Can't find you", "So Sad");
     }
-    
-  }  
+  };
 
-  componentDidMount(){
+  componentDidMount() {
     this.getLocation();
   }
-  render(){
-    const { isLoading } = this.state;
-    return isLoading ? <Loading /> : null;
+  render() {
+    const { isLoading, temp, condition } = this.state;
+    return isLoading ? (
+      <Loading />
+    ) : (
+      <Weather temp={Math.round(temp)} condition={condition} />
+    );
   }
 }
-  
